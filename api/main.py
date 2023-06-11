275c98aa2a4c269d6dacd9c2e53796f08cb44ec6
memuau <monocyklon@gmail.com>
2023-06-11 21:38:00 +0200

    changed env variables

--- api/main.py
+++ api/main.py
@@ -10,8 +10,8 @@ app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
+    app.mongodb_client = MongoClient(config["atlas-uri"])
+    app.database = app.mongodb_client[config["db-name"]]
 
 @app.on_event("shutdown")
 def shutdown_db_client():
