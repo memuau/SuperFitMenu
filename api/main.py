5db654b95fe0589229885de05cb835ff42426250
memuau <monocyklon@gmail.com>
2023-06-11 21:54:18 +0200

    fix CD

--- api/main.py
+++ api/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
-from dotenv import dotenv_values
+import os
 from pymongo import MongoClient
 from routes import ingredient_router, router3
 import uvicorn
 
-config = dotenv_values(".env")
+ATLAS_URI = os.environ['atlas-uri']
+DB_NAME = os.environ['db-name']
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient(config["atlas-uri"])
-    app.database = app.mongodb_client[config["db-name"]]
+    app.mongodb_client = MongoClient(ATLAS_URI)
+    app.database = app.mongodb_client[DB_NAME]
 
 @app.on_event("shutdown")
 def shutdown_db_client():
